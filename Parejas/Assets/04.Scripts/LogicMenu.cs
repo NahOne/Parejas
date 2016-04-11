80c0e4bcd34e955525b6528328e6720925bb6794
jonathanhr <jonathan.hortelano@gmail.com>
2016-04-11 17:11:43 +0200

    implementando menu

---
+++ Parejas/Assets/04.Scripts/LogicMenu.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LogicMenu : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    private void StartGame()
+    {
+        Application.LoadLevel(1);
+    }
+
+    private void ShowRanking()
+    {
+
+    }
+
+    private void Exit()
+    {
+        Application.Quit();
+    }
+}
