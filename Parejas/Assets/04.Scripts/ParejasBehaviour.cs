fb7583c4cdda17b66fdbfae7bc4d80e9d78eeae7
jonathanhr <jonathan.hortelano@gmail.com>
2016-04-13 16:38:10 +0200

    creando maquina estados

---
+++ Parejas/Assets/04.Scripts/ParejasBehaviour.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ParejasBehaviour : MonoBehaviour {
+    private ParejasBehaviourStateMachine m_ParejasSM = null;
+
+    // Use this for initialization
+    void Start () {
+        // create the state machine and start it
+        m_ParejasSM = new ParejasBehaviourStateMachine(this.gameObject);
+        m_ParejasSM.StartSM();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        // update the state machine very frame
+        if (m_ParejasSM != null)
+        {
+            m_ParejasSM.UpdateSM();
+        }
+
+        // this is how you can print the current active state tree to the log for debugging
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            m_ParejasSM.PrintActiveStateTree();
+        }
+    }
+}
---
+++ Parejas/Assets/04.Scripts/ParejasBehaviourStateMachine.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using FSMHelper;
+
+public class ParejasBehaviourStateMachine : FSMStateMachine {
+    public GameObject m_GameObject = null;
+
+    public ParejasBehaviourStateMachine(GameObject characterObj)
+    {
+        m_GameObject = characterObj;
+    }
+
+    // here we define the structure of the state machine's first layer
+    public override void SetupDefinition(ref FSMStateType stateType, ref List<System.Type> children)
+    {
+        // default is an ORtype state
+        // the first child added will be the inital state
+        children.Add(typeof(State_AssignAndShow));
+        children.Add(typeof(State_CheckTokens));
+        children.Add(typeof(State_ClueLife));
+        children.Add(typeof(State_CorrectTokens));
+        children.Add(typeof(State_FailTokens));
+        children.Add(typeof(State_GameFinished));
+        children.Add(typeof(State_WaitToken1));
+        children.Add(typeof(State_WaitToken2));
+        children.Add(typeof(State_WaitToPlay));
+    }
+}
